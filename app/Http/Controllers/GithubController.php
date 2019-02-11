65595877fe5834a2928883b94098f83b222e6796
Dotunj <dotunjolaosho@yahoo.com>
2019-02-11 17:56:59 +0100

    initial commit

---
+++ app/Http/Controllers/GithubController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class GithubController extends Controller
+{
+    public function fetchScoreFromUsername($username)
+    {
+        
+    }
+}
